9a7a9ad3d61d8069febb163a945cf4f3969d0705
ea7058 <a66967280@gmail.com>
2026-02-05 21:32:38 +0300

    Create register.js

--- js/register.js
+++ js/register.js
@@ -1,1 +1,23 @@
+function register() {
+  const u = document.getElementById("username").value;
+  const p1 = document.getElementById("password").value;
+  const p2 = document.getElementById("password2").value;
+  const rules = document.getElementById("rules").checked;
 
+  if (!u || !p1 || !p2) {
+    alert("Boş bırakma");
+    return;
+  }
+
+  if (p1 !== p2) {
+    alert("Şifreler aynı değil");
+    return;
+  }
+
+  if (!rules) {
+    alert("Kuralları kabul et");
+    return;
+  }
+
+  alert("Kayıt başarılı (şimdilik demo)");
+}
